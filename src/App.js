bf04a63ddb29933bd6bfa5327bd42b8c14bfd110
David <davidspura6@gmail.com>
2020-02-17 13:09:37 +0100

    updating project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React, { Component } from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "moment/locale/cs";
 import moment from "moment";
-import CanvasJSReact from "./canvasjs.react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 export class App extends Component {
   state = {
     data: [],
-    startDate: new Date()
+    startDate: new Date(),
+    API: "https://api.testing.barkio.com/api/stats/installs"
   };
 
+  static jsfiddleUrl = "https://jsfiddle.net/alidingling/9hjfkp73/";
+
   componentDidMount() {
-    fetch("https://api.testing.barkio.com/api/stats/installs")
+    fetch(this.state.API)
       .then(res => res.json())
       .then(res => {
         this.setState({ data: res });
@@ -36,15 +46,20 @@ export class App extends Component {
       startDate: date
     });
     const kalendar = moment(date).format("YYYY-MM-DD");
-    const kalendarGet = `?lastDate=${kalendar}`;
-    fetch("https://api.testing.barkio.com/api/stats/installs" + kalendarGet);
+    let newAPI = "";
+    newAPI = `${this.state.API}?lastDate=${kalendar}`;
+    fetch(newAPI)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({ data: res });
+      });
   };
 
   render() {
     const newData = [...this.state.data];
     const android = newData.map(data => {
       return {
-        label: moment(data.createdAt).calendar(null, {
+        name: moment(data.createdAt).calendar(null, {
           sameDay: "[Dnes]",
           nextDay: "[Zítra]",
           nextWeek: "Do Mo YYYY dddd",
@@ -52,68 +67,13 @@ export class App extends Component {
           lastWeek: "Do Mo YYYY dddd",
           sameElse: "Do Mo YYYY dddd"
         }),
-        y: data.androidTotal
+        iOS: data.iosTotal,
+        Android: data.androidTotal,
+        Celkem: data.androidTotal + data.iosTotal
       };
     });
-    const ios = newData.map(data => {
-      return {
-        label: "",
-        y: data.iosTotal
-      };
-    });
-    const celkem = newData.map(data => {
-      return {
-        label: "",
-        y: data.iosTotal + data.androidTotal
-      };
-    });
-    console.log(this.state.data);
-    const options = {
-      animationEnabled: true,
-      exportEnabled: true,
-      title: {
-        text: "Počet stažení za posledních 7 dní",
-        fontFamily: "verdana"
-      },
-      axisY: {
-        title: "Počet zařízení",
-        labelFormatter: function(e) {
-          return e.value;
-        }
-      },
-      toolTip: {
-        shared: true,
-        reversed: true
-      },
-      legend: {
-        verticalAlign: "center",
-        horizontalAlign: "right",
-        reversed: true,
-        cursor: "pointer",
-        itemclick: this.toggleDataSeries
-      },
-      data: [
-        {
-          type: "stackedColumn",
-          name: "iOS",
-          showInLegend: true,
-          dataPoints: ios
-        },
-        {
-          type: "stackedColumn",
-          name: "Android",
-          showInLegend: true,
-          dataPoints: android
-        },
-        {
-          type: "stackedColumn",
-          name: "Celkem",
-          axisYType: "secondary",
-          showInLegend: true,
-          dataPoints: celkem
-        }
-      ]
-    };
+    const data = android;
+
     return !this.state.data.length ? (
       <div className="container align-center">
         <DatePicker
@@ -123,13 +83,35 @@ export class App extends Component {
         <h1 className="text-center">Žádná data k zobrazení</h1>
       </div>
     ) : (
-      <div className="container">
-        <DatePicker
-          selected={this.state.startDate}
-          onChange={this.handleChange}
-        />
-        <CanvasJSChart options={options} onRef={ref => (this.chart = ref)} />
-        <div></div>
+      <div>
+        <div className="container">
+          <DatePicker
+            selected={this.state.startDate}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div className="container d-flex justify-content-center">
+          <BarChart
+            width={1200}
+            height={300}
+            data={data}
+            margin={{
+              top: 20,
+              right: 20,
+              left: 20,
+              bottom: 5
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="iOS" stackId="a" fill="#8884d8" />
+            <Bar dataKey="Android" stackId="a" fill="#82ca9d" />
+            <Bar dataKey="Celkem" fill="#ffc658" />
+          </BarChart>
+        </div>
       </div>
     );
   }
